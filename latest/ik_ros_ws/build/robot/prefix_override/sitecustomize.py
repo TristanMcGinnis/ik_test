c7741e82078ae6f32c86543a9880426f0beef598
Tristan McGinnis <tristanmcginnis26@gmail.com>
2025-01-31 18:16:23 -0600

    Remove all build-related folders

--- latest/ik_ros_ws/build/robot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/tristan/astra/ik_test/latest/ik_ros_ws/install/robot'
--- latest/ik_ros_ws/build/robot/share/robot/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/tristan/Desktop/ik_test/latest/ik_ros_ws/build/robot"
